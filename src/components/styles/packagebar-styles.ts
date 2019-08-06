cbf7674da0a6790c6597e6631d29629f948bfb17
marisaaquilina <marisa.aquilina@gmail.com>
2019-08-06 16:53:55 -0700

    Added setProgress state

--- src/components/styles/packagebar-styles.ts
+++ src/components/styles/packagebar-styles.ts
@@ -2,14 +2,25 @@ import { style } from 'typestyle';
 
 export namespace PackageBarStyleClasses {
   export const packageContainer = style({
-    backgroundColor: 'var(--md-blue-700)'
+    backgroundColor: '#F2F2F2',
+    padding: 20,
   });
   export const pipButton = style({
     backgroundColor: 'var(--md-blue-700)',
     color: '#fff',
     border: 'none',
-    padding: '2px',
-    width: '20px',
+    padding: '2px 10px',
     margin: 2
+  });
+  export const packageInput = style({
+    border: '1px solid var(--md-grey-400)',
+    padding: 8,
+  });
+  export const packageLabel = style({
+    textTransform: 'uppercase',
+  });
+  export const title = style({
+    fontSize: 16,
+    fontWeight: 'bold',
   });
 }
