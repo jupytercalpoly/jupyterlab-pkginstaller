20deecd6d8ba2d1b5e9886cda3aff71e87f77e96
marisaaquilina <marisa.aquilina@gmail.com>
2019-08-08 20:44:11 -0700

    Added searchTitle styles and installation feedback

--- src/style/packagebar-styles.ts
+++ src/style/packagebar-styles.ts
@@ -9,18 +9,22 @@ export namespace PackageBarStyleClasses {
     borderRadius: '3px',
     margin: 'auto',
   });
+  export const topBar = style({
+    display: 'flex',
+    justifyContent: 'space-between',
+  })
   export const pipButton = style({
     backgroundColor: 'var(--md-blue-700)',
     color: '#f2f2f2',
     border: 'none',
     padding: '8px 0px',
-    margin: 2,
+    margin: 0,
     borderRadius: '2px',
   });
   export const packageInput = style({
     border: '1px solid var(--md-grey-400)',
     padding: 8,
-    marginRight: 8,
+    boxSizing: 'border-box',
   });
   export const packageLabel = style({
     textTransform: 'uppercase',
@@ -39,10 +43,30 @@ export namespace PackageBarStyleClasses {
     display: 'flex',
     flexDirection: 'column',
   });
+  export const searchTitle = style({
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    minHeight: '2.5em',
+    textTransform: 'uppercase',
+    letterSpacing: '0.06em',
+    color: '#4F4F4F',
+    fontWeight: 'bold',
+    fontSize: '10.88px',
+    lineHeight: '13px',
+    marginBottom: '0.75em',
+  });
   export const buttonContainer = style({
     marginTop: '0.5em',
     display: 'grid',
     gridTemplateColumns: '1fr 1fr',
     gridGap: '0.5em',
+  });
+  export const loading = style({
+    background: 'none',
+    backgroundRepeat: 'no-repeat',
+    backgroundPositionX:'98%',
+    backgroundPositionY: '50%',
+    backgroundSize: '20px',
   });
 }
