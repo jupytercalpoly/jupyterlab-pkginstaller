e4f7fceb54b710d3da7be39d24c570884dcd126b
marisaaquilina <marisa.aquilina@gmail.com>
2019-08-29 09:24:17 -0700

    Fixed kernel issue

--- src/Widget.tsx
+++ src/Widget.tsx
@@ -13,14 +13,8 @@ import { Widget, BoxLayout, PanelLayout } from '@phosphor/widgets';
 import { KernelSpyModel, ThreadIterator } from './model';
 
 import '../style/index.css';
-//import ReactDOM from 'react-dom';
-// function Welcome() {
-//   return <h1>{Math.random().toString(36).substring(7)}</h1>;
-// }
 
 import { PackageSearcher } from './PackageBar';
-
-
 
 function Message(props: any): any {
   const msg = props.message;
@@ -28,9 +22,7 @@ function Message(props: any): any {
     <PackageSearcher kernelId={props.kernelId} kernelName={props.kernelName} uninstalledPackage={msg.content.evalue.split("'")[1]} moduleError={props.moduleError} layouty={props.layouty}/>
   );
 }
-// function Messagey(props: any): any {
-//   return <div>asdasd</div>;
-// }
+
 export class MessageLogView extends VDomRenderer<KernelSpyModel> {
   constructor(model: KernelSpyModel, kernelId: string, kernelName: string, layouty: PanelLayout) {
     super();
@@ -46,7 +38,7 @@ export class MessageLogView extends VDomRenderer<KernelSpyModel> {
     each(threads, ({args, hasChildren}) => {
       if (args.msg.header.msg_type=="error") {
         elements = [Message({ 
-          message:args.msg, kernelName: this.kernelName, kernelid: this.kernelId, moduleError: true, layouty: this.layouty
+          message:args.msg, kernelName: this.kernelName, kernelId: this.kernelId, moduleError: true, layouty: this.layouty
         })];
       }
     });
@@ -62,9 +54,6 @@ export class KernelSpyView extends Widget {
   constructor(kernel: Kernel.IKernel) {
     super();
     this._model = new KernelSpyModel(kernel);
-    //log("Model log", this._model.log[this._model.log.length - 1]);
-    //this._messagelog = new MessageLogView(this._model, 'a', 'a', 'a' as PanelLayout);
-    //console.log("Elemento", this._messagelog);
     this.addClass('jp-kernelspy-view');
     this.id = `kernelspy-${kernel.id}`;
     this.title.label = 'Show errors';
@@ -72,9 +61,7 @@ export class KernelSpyView extends Widget {
     this._toolbar = new Toolbar();
     this._toolbar.addClass('jp-kernelspy-toolbar');
     layout.addWidget(this._toolbar);
-    //layout.addWidget(this._messagelog);
     BoxLayout.setStretch(this._toolbar, 0);
-    //BoxLayout.setStretch(this._messagelog, 1);
     
   }
   get messageLog(): MessageLogView {
