bb086c9f22b4f9a4bb9f254da2d8aa8a731552d2
marisaaquilina <marisa.aquilina@gmail.com>
2019-08-29 17:53:30 -0700

    Resolved widget

--- src/Widget.tsx
+++ src/Widget.tsx
@@ -1,22 +1,18 @@
-'use strict';
+import { each } from '@phosphor/algorithm';
 
-import * as React from 'react';
+import { PanelLayout } from '@phosphor/widgets';
 
 import { VDomRenderer } from '@jupyterlab/apputils';
 
-import { Kernel } from '@jupyterlab/services';
+import * as React from 'react';
 
-import { each } from '@phosphor/algorithm';
-
-import { Widget, PanelLayout } from '@phosphor/widgets';
-
-import { KernelSpyModel, ThreadIterator } from './model';
-
-import '../style/index.css';
+import { KernelSpyModel, ThreadIterator } from './Model';
 
 import { PackageSearcher } from './PackageBar';
 
-function Message(props: any): any {
+import '../style/index.css';
+
+function Message(props: any) {
   const packagefromMessage = props.message.content.evalue.split("'")[1];
   return (
     <PackageSearcher kernelId={props.kernelId} kernelName={props.kernelName} uninstalledPackage={packagefromMessage} moduleError={props.moduleError} layouty={props.layouty}/>
@@ -31,9 +27,9 @@ export class MessageLogView extends VDomRenderer<KernelSpyModel> {
     this.kernelId = kernelId;
     this.layouty = layouty;
   }
-  protected render(): React.ReactElement<any>[] {
+  protected render(): React.ReactElement<any> {
     const model = this.model!;
-    let elements: React.ReactElement<any>[] = [];
+    let errorMessage: React.ReactElement<any>;
     let threads = new ThreadIterator(model.tree, this.collapsed);
     each(threads, ({args, hasChildren}) => {
       if (args.msg.header.msg_type=="error") {
@@ -41,12 +37,12 @@ export class MessageLogView extends VDomRenderer<KernelSpyModel> {
         if (count > 1) {
           this.layouty.widgets[count - 1].dispose();  
         }
-        elements = [Message({ 
+        errorMessage = Message({ 
           message:args.msg, kernelName: this.kernelName, kernelId: this.kernelId, moduleError: true, layouty: this.layouty
-        })];
+        });
       }
     });
-    return [elements[0]];
+    return errorMessage;
   }
   protected collapsed: {[key: string]: boolean} = {};
   protected kernelName: string;
@@ -54,28 +50,3 @@ export class MessageLogView extends VDomRenderer<KernelSpyModel> {
   protected layouty: PanelLayout;
 }
 
-export class KernelSpyView extends Widget {
-  constructor(kernel: Kernel.IKernel) {
-    super();
-    //this._model = new KernelSpyModel(kernel);
-    // this.addClass('jp-kernelspy-view');
-    // this.id = `kernelspy-${kernel.id}`;
-    // this.title.label = 'Show errors';
-    // let layout = this.layout = new BoxLayout();
-    // this._toolbar = new Toolbar();
-    // this._toolbar.addClass('jp-kernelspy-toolbar');
-    // layout.addWidget(this._toolbar);
-    // BoxLayout.setStretch(this._toolbar, 0);
-    
-  }
-  // get messageLog(): MessageLogView {
-  //   return this._messagelog;
-  // }
-
-  // get model(): KernelSpyModel {
-  //   return this._model;
-  // }
-  // //private _toolbar: Toolbar<Widget>;
-  // private _messagelog: MessageLogView;
-  // private _model: KernelSpyModel;
-}
