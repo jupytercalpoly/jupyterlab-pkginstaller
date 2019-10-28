1d5789f3a70e0c1e96d93d540c8bc3aac4d9bcf3
marisaaquilina <marisa.aquilina@gmail.com>
2019-10-28 14:37:18 -0700

    Integrating new panel

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import {
-  JupyterFrontEnd, JupyterFrontEndPlugin
+  JupyterFrontEnd, JupyterFrontEndPlugin,  ILabShell
 } from '@jupyterlab/application';
 
 import {
@@ -8,21 +8,43 @@ import {
 
 import PackageTool from './PackageTool';
 
+
+import { Widget } from "@phosphor/widgets";
+
 import '../style/index.css';
 
 /**
  * Initialization data for the pkginstaller extension.
  */
-const pkginstaller: JupyterFrontEndPlugin<void> = {
-  id: 'pkginstaller',
-  autoStart: true,
-  requires: [INotebookTools, INotebookTracker],
-  activate: (app: JupyterFrontEnd, cellTools: INotebookTools, notebookTracker: INotebookTracker) => {  
-    const packageTool = new PackageTool(app, notebookTracker);
-    cellTools.addItem({ tool: packageTool });
-  }
-};
+// const pkginstaller: JupyterFrontEndPlugin<void> = {
+//   id: 'pkginstaller',
+//   autoStart: true,
+//   requires: [INotebookTools, INotebookTracker],
+//   activate: (app: JupyterFrontEnd, cellTools: INotebookTools, notebookTracker: INotebookTracker) => {  
+//     const packageTool = new PackageTool(app, notebookTracker);
+//     cellTools.addItem({ tool: packageTool });
+//   }
+// };
 
-export default [
-  pkginstaller
-];
+export default {
+  activate,
+  id: "@jupyterlab-packageinstaller",
+  requires: [ILabShell, INotebookTools, INotebookTracker],
+  autoStart: true
+} as JupyterFrontEndPlugin<any>;
+
+function activate(
+  app: JupyterFrontEnd,
+  labShell: ILabShell,
+  notebookTracker: INotebookTracker
+): any {
+  
+  const widget : Widget = new PackageTool(app, notebookTracker);
+  widget.title.iconClass = "jp-SpreadsheetIcon jp-SideBar-tabIcon";
+  widget.title.caption = "Package Installer";
+  labShell.add(widget, "left");
+}
+
+// export default [
+//   pkginstaller
+// ];
