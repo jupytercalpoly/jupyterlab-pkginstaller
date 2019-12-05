71fe0fd912743e32b6d28df3283e72bb9c433045
marisaaquilina <marisa.aquilina@gmail.com>
2019-12-04 18:15:09 -0800

    Removed unecess plugin

--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,15 @@ import {
   JupyterFrontEnd, JupyterFrontEndPlugin,  ILabShell, ILayoutRestorer
 } from '@jupyterlab/application';
 
-//import { ReactWidget } from "@jupyterlab/apputils";
 import { Widget } from '@phosphor/widgets';
 
 import {
-  INotebookTracker, INotebookTools//NotebookPanel // INotebookTools,
+  INotebookTracker, INotebookTools
 } from '@jupyterlab/notebook';
 
 import PackageTool from './PackageTool';
+
 import PInstallerWidget from './PackageInstaller';
-
-//import { KernelSpyView } from './KernelSpyView';
-//import { MessageLogView } from './Logs';
-//import { Kernel } from '@jupyterlab/services';
-
-//import PackageSearcher from './PackageBar';
-// import {
-//   IStatusBar,
-//   KernelStatus
-// } from '@jupyterlab/statusbar';
-
-// import { IClientSession } from '@jupyterlab/apputils';
-
-// import { Title } from '@phosphor/widgets';
-// import {
-//   ConsolePanel,
-//   IConsoleTracker
-// } from '@jupyterlab/console';
 
 import '../style/index.css';
 
@@ -63,110 +45,17 @@ function activate(
   notebookTracker: INotebookTracker
 ): void {
 
-  // Create a dataset with this URL
-  //const newwidget = new PackageTool(app, notebookTracker); newwidget;
-  // console.log(notebookTracker.currentWidget.session.kernel.id);
   let widget: Widget = new PInstallerWidget(notebookTracker);
-  //let layout = new PanelLayout();
   widget.id = "@jupyterlab-pkginstaller";
   widget.title.iconClass = "jp-PackageInstaller-icon jp-SideBar-tabIcon";
   widget.title.caption = "Package Installer";
   restorer.add(widget, widget.id);
   labShell.add(widget, "left");
   notebookTracker.currentChanged.connect(() => {
-    // console.log(notebookTracker);
-    //let session = notebookTracker.currentWidget.session;
-    // let widget1 = new KernelSpyView(notebookTracker.currentWidget.context.session.kernel! as Kernel.IKernel);
-    // (app.shell).add(widget1);
-    // view;
     widget.update();
   });
 
 }
-
-// /**
-//  * A plugin that provides a kernel status item to the status bar.
-//  */
-// export const kernelStatus: JupyterFrontEndPlugin<void> = {
-//   id: '@jupyterlab/statusbar-extension:kernel-status1',
-//   autoStart: true,
-//   requires: [IStatusBar, INotebookTracker, IConsoleTracker, ILabShell],
-//   activate: (
-//     app: JupyterFrontEnd,
-//     statusBar: IStatusBar,
-//     notebookTracker: INotebookTracker,
-//     consoleTracker: IConsoleTracker,
-//     labShell: ILabShell
-//   ) => {
-//     // When the status item is clicked, launch the kernel
-//     // selection dialog for the current session.
-//     let currentSession: IClientSession | null = null;
-//     const changeKernel = async () => {
-//       if (!currentSession) {
-//         return;
-//       }
-//       await currentSession.selectKernel();
-//     };
-
-//     currentSession!.kernelChanged.connect(() => {
-//       void currentSession.kernel.ready.then(() => {
-//         console.log("kernel was changed");
-//       });
-//     });
-
-//     // Create the status item.
-//     const item = new KernelStatus({
-//       onClick: changeKernel
-//     });
-
-//     // When the title of the active widget changes, update the label
-//     // of the hover text.
-//     const onTitleChanged = (title: Title<Widget>) => {
-//       item.model!.activityName = title.label;
-//       console.log(item.model!.session);
-//     };
-
-//     // Keep the session object on the status item up-to-date.
-//     labShell.currentChanged.connect((_, change) => {
-//       const { oldValue, newValue } = change;
-
-//       // Clean up after the old value if it exists,
-//       // listen for changes to the title of the activity
-//       if (oldValue) {
-//         oldValue.title.changed.disconnect(onTitleChanged);
-//       }
-//       if (newValue) {
-//         newValue.title.changed.connect(onTitleChanged);
-//       }
-
-//       // Grab the session off of the current widget, if it exists.
-//       if (newValue && consoleTracker.has(newValue)) {
-//         currentSession = (newValue as ConsolePanel).session;
-//       } else if (newValue && notebookTracker.has(newValue)) {
-//         currentSession = (newValue as NotebookPanel).session;
-//       } else {
-//         currentSession = null;
-//       }
-//       item.model!.session = currentSession;
-//     });
-
-//     statusBar.registerStatusItem(
-//       '@jupyterlab/statusbar-extension:kernel-status1',
-//       {
-//         item,
-//         align: 'left',
-//         rank: 1,
-//         isActive: () => {
-//           const current = labShell.currentWidget; 
-//           return (
-//             current &&
-//             (notebookTracker.has(current) || consoleTracker.has(current))
-//           );
-//         }
-//       }
-//     );
-//   }
-// };
 
 export default [
   panelly, pkginstaller //kernelStatus, //pkginstaller
---
+++ src/style/packageinstaller-styles.ts
@@ -0,0 +1,162 @@
+import { style } from 'typestyle';
+
+export namespace PackageInstallerStyleClasses {
+  export const packageContainer = style({
+    padding: '2em 1em',
+    display: 'flex',
+    flexDirection: 'column',
+  });
+  export const topBar = style({
+    display: 'flex',
+    justifyContent: 'space-between',
+    paddingTop: 10,
+    fontSize: 'var(--jp-ui-font-size1)'
+  });
+  export const kernelPrompt = style({
+    paddingTop: 5,
+  })
+  export const pipButton = style({
+    backgroundColor: 'var(--md-blue-700)',
+    color: 'var(--jp-ui-inverse-font-color1)',
+    border: 'none',
+    padding: '8px 0px',
+    margin: 0,
+    borderRadius: '2px',
+    fontSize: 'var(--jp-ui-font-size1)',
+  });
+  export const snackbar = style({
+    backgroundColor: 'red',
+  });
+  export const pipInstallButton = style({
+    backgroundColor: 'var(--md-blue-700)',
+    color: 'var(--jp-ui-inverse-font-color1)',
+    border: 'none',
+    padding: '8px 0px',
+    margin: 0,
+    float: 'right',
+    borderRadius: '2px',
+    width: '40px'
+  });
+  export const errorButton = style({
+    backgroundColor: 'white',
+    color: 'var(--jp-ui-inverse-font-color1)',
+    border: 'none',
+    boxShadow: 'none',
+  });
+  export const errorContainer = style({
+    backgroundColor: 'var(--md-orange-100)',
+    border: '1px solid var(--md-orange-300)',
+    padding: '8px',
+    display: 'flex',
+    flexDirection: 'column',
+  });
+  export const errorContainerActions = style({
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'baseline'
+  });
+  export const uninstalledPackage = style({
+    backgroundColor: 'var(--md-grey-200)',
+    padding: '2px 4px',
+    color: 'var(--jp-ui-inverse-font-color1)'
+  });
+  export const packageInput = style({
+    border: '1px solid var(--md-grey-400)',
+    padding: 8,
+    boxSizing: 'border-box',
+    fontSize: 'var(--jp-ui-font-size1)',
+  });
+  export const switchText = style({
+    fontSize: 'var(--jp-ui-font-size0)',
+  });
+  export const switchAccent = style({
+    fontSize: 'var(--jp-ui-font-size0)',
+    textTransform: 'uppercase',
+    color: 'var(--jp-ui-font-color2)',
+
+  });
+  export const PIComponent = style({
+    background: "var(--jp-layout-color1)",
+    minWidth: "var(--jp-sidebar-min-width)",
+    overflow: "visible",
+    display: "flex",
+    flex: "0 0 auto",
+    height: '100vh',
+  });
+  export const PIComponentHeader = style({
+    fontSize: "var(--jp-ui-font-size0)",
+    fontWeight: 600,
+    letterSpacing: 1,
+    margin: 0,
+    padding: "8px 12px",
+    textTransform: "uppercase",
+    display: "block"
+  })
+  export const logsButton = style({
+    backgroundColor: 'none',
+    color: 'var(--md-blue-700)',
+    border: 'none',
+    padding: '8px',
+    margin: 0,
+    marginTop: 4,
+    borderRadius: 2,
+    float: 'right',
+    fontSize: 'var(--jp-ui-font-size1)',
+  });
+  export const packageLabel = style({
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    fontSize: 10,
+    paddingBottom: 2,
+  });
+  export const title = style({
+    fontWeight: 'bold',
+    fontSize: 'var(--jp-ui-font-size3)',
+    lineHeight: '21px',
+    color: 'var(--jp-ui-font-color1)',
+  });
+  export const search = style({
+    marginTop: '1em',
+    display: 'flex',
+    flexDirection: 'column',
+  });
+  export const messageText = style({
+    color: 'var(--jp-ui-font-color2)',
+    fontWeight: 'bold',
+    fontSize: '10.88px',
+    lineHeight: '13px',
+    marginBottom: '0.75em',
+  });
+  export const searchTitle = style({
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    minHeight: '2.5em',
+    textTransform: 'uppercase',
+    letterSpacing: '0.06em',
+    color: 'var(--jp-ui-font-color2)',
+    fontWeight: 'bold',
+    fontSize: 'var(--jp-ui-font-size0)',
+    lineHeight: '13px',
+    marginBottom: '0.75em',
+  });
+  export const heading = style({
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    minHeight: '2.5em',
+  });
+  export const buttonContainer = style({
+    marginTop: '0.5em',
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    gridGap: '0.5em',
+  });
+  export const loading = style({
+    background: 'none',
+    backgroundRepeat: 'no-repeat',
+    backgroundPositionX:'98%',
+    backgroundPositionY: '50%',
+    backgroundSize: '20px',
+  });
+}
