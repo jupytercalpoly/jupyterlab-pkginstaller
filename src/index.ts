c8725fc3a4033c3e227ff446e2d18bd58d240a8c
marisaaquilina <marisa.aquilina@gmail.com>
2019-12-04 18:45:43 -0800

    Consolidated initializations in index.ts

--- src/index.ts
+++ src/index.ts
@@ -20,42 +20,29 @@ import '../style/index.css';
 const pkginstaller: JupyterFrontEndPlugin<void> = {
   id: '@jupyterlab/pkginstaller-extension',
   autoStart: true,
-  requires: [INotebookTools, INotebookTracker],
-  activate: (app: JupyterFrontEnd, cellTools: INotebookTools, notebookTracker: INotebookTracker) => {  
+  requires: [INotebookTools, INotebookTracker, ILabShell, ILayoutRestorer, INotebookTracker],
+  activate: (
+    app: JupyterFrontEnd, 
+    cellTools: INotebookTools, 
+    notebookTracker: INotebookTracker,
+    labShell: ILabShell,
+    restorer: ILayoutRestorer,
+  ) => {  
+    let widget: Widget = new PInstallerWidget(notebookTracker);
+    widget.id = "@jupyterlab-pkginstaller";
+    widget.title.iconClass = "jp-PackageInstaller-icon jp-SideBar-tabIcon";
+    widget.title.caption = "Package Installer";
+    restorer.add(widget, widget.id);
+    labShell.add(widget, "left");
+
     const packageTool = new PackageTool(app, notebookTracker);
     cellTools.addItem({ tool: packageTool });
+    notebookTracker.currentChanged.connect(() => {
+      widget.update();
+    });
   }
 };
 
-const id = "@jupyterlab/dataregistry-extension:data-explorer";
-/**
- * Adds a visual data explorer to the sidebar.
- */
-const panelly: JupyterFrontEndPlugin<void> = {
-  activate,
-  id,
-  requires: [ILabShell, ILayoutRestorer, INotebookTracker],
-  autoStart: true
-} as JupyterFrontEndPlugin<void>;
-
-function activate(
-  app: JupyterFrontEnd,
-  labShell: ILabShell,
-  restorer: ILayoutRestorer,
-  notebookTracker: INotebookTracker
-): void {
-
-  let widget: Widget = new PInstallerWidget(notebookTracker);
-  widget.id = "@jupyterlab-pkginstaller";
-  widget.title.iconClass = "jp-PackageInstaller-icon jp-SideBar-tabIcon";
-  widget.title.caption = "Package Installer";
-  restorer.add(widget, widget.id);
-  labShell.add(widget, "left");
-  notebookTracker.currentChanged.connect(() => {
-    widget.update();
-  });
-}
-
 export default [
-  panelly, pkginstaller 
+  pkginstaller 
 ];
